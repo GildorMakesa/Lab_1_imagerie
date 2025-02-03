d4908decd0b9f0910426334d71a02a7fc3c50525
astro <astro@rick>
2025-02-03 11:50:37 -0500

    partt 3 re-added

--- GTI411 - LAB 1/Lab1/models/transforms.py
+++ GTI411 - LAB 1/Lab1/models/transforms.py
@@ -1,4 +1,6 @@
-
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 
 class Lab1ImageTransformsModel:
     
@@ -13,5 +15,18 @@ class Lab1ImageTransformsModel:
 
 
     def transform_image(self, contrast, luminosity):
-        # TODO
-        return self.image
+        hsv_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV) #so opencv can play with the image
+        
+        hsv_image = hsv_image.astype(np.float32) #opencv arithmetic conversion
+        
+        hsv_image[..., 1] = hsv_image[..., 1] * contrast #Apply contrast to S (saturation of HSV)
+        hsv_image[..., 1] = np.clip(hsv_image[..., 1], 0, 255)
+
+        hsv_image[..., 2] = hsv_image[..., 2] + luminosity #Apply luminosity to v (value of HSV)
+        hsv_image[..., 2] = np.clip(hsv_image[..., 2], 0, 255)
+        
+        hsv_image = hsv_image.astype(np.uint8) #opencv arithmetic conversion
+        
+        transformed_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR) #RGB conversion
+        
+        return transformed_image
