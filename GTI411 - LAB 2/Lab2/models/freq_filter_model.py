3ec66e6f60285a6afd32957179dfd7ca4585b8e4
oli-gh <113401495+oli-gh@users.noreply.github.com>
2025-02-23 19:23:13 -0500

    part3 done !

--- GTI411 - LAB 2/Lab2/models/freq_filter_model.py
+++ GTI411 - LAB 2/Lab2/models/freq_filter_model.py
@@ -36,23 +36,14 @@ class FrequencyFilterModel:
 
         print(f"Applying with ideal lowpass with:  {self.cutoff_freq}")
 
-
-
-
-        dft_shift = transforme_de_fourier(image)
-        masque = creer_masque_passe_bas(image, self.cutoff_freq)
-        dft_shift_filtered = appliquer_masque(dft_shift, masque)
+        imge_2_tf = transforme_de_fourier(image)
+        filtre = ideal_lowpass_filter(image, self.cutoff_freq)
+        tf_2_filtre = apply_filtre(imge_2_tf, filtre)
 
    
-        original_image_spectrum = calculer_spectre_magnitude(dft_shift)
-        ideal_filter_spectrum = calculer_spectre_magnitude(dft_shift_filtered)
-
-
-        ideal_filter_recons = appliquer_filtre(dft_shift_filtered)
-
-
-
-
+        original_image_spectrum = calculer_spectre_magnitude(imge_2_tf)
+        ideal_filter_spectrum = calculer_spectre_magnitude(tf_2_filtre)
+        ideal_filter_recons = transforme_de_fourier_inverse(tf_2_filtre)
 
         images = {
             'original_spectrum': original_image_spectrum,
@@ -69,9 +60,14 @@ class FrequencyFilterModel:
 
         print(f"Applying with ideal highpass with: {self.cutoff_freq}")
 
-        original_image_spectrum = create_fake_image(image, "Orig spectrum")
-        ideal_filter_spectrum = create_fake_image(image, "Ideal spectrum") 
-        ideal_filter_recons = create_fake_image(image, "Ideal recons") 
+        imge_2_tf = transforme_de_fourier(image)
+        filtre = ideal_highpass_filter(image, self.cutoff_freq)
+        tf_2_filtre = apply_filtre(imge_2_tf, filtre)
+
+   
+        original_image_spectrum = calculer_spectre_magnitude(imge_2_tf)
+        ideal_filter_spectrum = calculer_spectre_magnitude(tf_2_filtre)
+        ideal_filter_recons = transforme_de_fourier_inverse(tf_2_filtre)
 
         images = {
             'original_spectrum': original_image_spectrum,
@@ -88,9 +84,13 @@ class FrequencyFilterModel:
 
         print(f"Applying with Butter lowpass with: {self.n_params_butter}")
 
-        original_image_spectrum = create_fake_image(image, "Orig spectrum")
-        butter_filter_spectrum = create_fake_image(image, "Butter spectrum") 
-        butter_filter_recons = create_fake_image(image, "Butter recons") 
+        imge_2_tf = transforme_de_fourier(image)
+        filtre = butterworth_lowpass_filter(image, self.cutoff_freq, self.n_params_butter)
+        tf_2_filtre = apply_filtre(imge_2_tf, filtre)
+
+        original_image_spectrum = calculer_spectre_magnitude(imge_2_tf)
+        butter_filter_spectrum = calculer_spectre_magnitude(tf_2_filtre)
+        butter_filter_recons = transforme_de_fourier_inverse(tf_2_filtre)
 
         images = {
             'original_spectrum': original_image_spectrum,
@@ -107,9 +107,13 @@ class FrequencyFilterModel:
 
         print(f"Applying with Butter highpass with: {self.n_params_butter}")
 
-        original_image_spectrum = create_fake_image(image, "Orig spectrum")
-        butter_filter_spectrum = create_fake_image(image, "Butter spectrum") 
-        butter_filter_recons = create_fake_image(image, "Butter recons") 
+        imge_2_tf = transforme_de_fourier(image)
+        filtre = butterworth_highpass_filter(image, self.cutoff_freq, self.n_params_butter)
+        tf_2_filtre = apply_filtre(imge_2_tf, filtre)
+
+        original_image_spectrum = calculer_spectre_magnitude(imge_2_tf)
+        butter_filter_spectrum = calculer_spectre_magnitude(tf_2_filtre)
+        butter_filter_recons = transforme_de_fourier_inverse(tf_2_filtre)
 
         images = {
             'original_spectrum': original_image_spectrum,
@@ -118,60 +122,92 @@ class FrequencyFilterModel:
         }
         return images
     
-################################################################################
-
-
 
 def transforme_de_fourier(image):
-    spectre_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    channels = cv2.split(image)
     
-    dft = np.fft.fft2(spectre_gray)
-    dft_shift = np.fft.fftshift(dft)
+    dft_channels = []
+    for channel in channels:
+        dft = np.fft.fft2(channel)
+        dft_shift = np.fft.fftshift(dft)
+        dft_channels.append(dft_shift)
     
-    return dft_shift
+    return dft_channels
 
-def appliquer_masque(dft_shift, masque):
-    return dft_shift * masque
+def apply_filtre(dft_shift_channels, filtre):
+    dft_shift_filtered = [dft_shift * filtre for dft_shift in dft_shift_channels]
+    return dft_shift_filtered
 
-def calculer_spectre_magnitude(dft_shift_filtered):
-    magnitude_spectrum = np.abs(dft_shift_filtered)
-    magnitude_spectrum = np.log(magnitude_spectrum + 1)
-    magnitude_spectrum = np.uint8(255 * magnitude_spectrum / np.max(magnitude_spectrum))
+def calculer_spectre_magnitude(dft_shift_filtered_channels):
+    magnitude_spectra = []
+
+    for dft_shift_filtered in dft_shift_filtered_channels:
+        magnitude_spectrum = np.abs(dft_shift_filtered)
+        magnitude_spectrum = np.log(magnitude_spectrum + 1)
+        magnitude_spectrum = np.uint8(255 * magnitude_spectrum / np.max(magnitude_spectrum))
+        magnitude_spectra.append(magnitude_spectrum)
     
-    return cv2.merge([magnitude_spectrum, magnitude_spectrum, magnitude_spectrum])
+    return cv2.merge(magnitude_spectra)
 
-def creer_masque_passe_bas(image, cutoff_freq):
+def ideal_lowpass_filter(image, cutoff_freq):
     rows, cols, _ = image.shape
-
-    masque = np.zeros((rows, cols), dtype=np.float32)
+    filtre = np.zeros((rows, cols), dtype=np.float32)
     crow, ccol = rows // 2, cols // 2
     
     for i in range(rows):
         for j in range(cols):
             distance = np.sqrt((i - crow)**2 + (j - ccol)**2)
             if distance <= cutoff_freq:
-                masque[i, j] = 1 
+                filtre[i, j] = 1
     
-    return masque
+    return filtre
 
-def creer_masque_passe_haut(rows, cols, cutoff_freq):
-    masque = np.ones((rows, cols), dtype=np.float32)
+def ideal_highpass_filter(image, cutoff_freq):
+    rows, cols, _ = image.shape
+    filtre = np.ones((rows, cols), dtype=np.float32)
     crow, ccol = rows // 2, cols // 2
     
     for i in range(rows):
         for j in range(cols):
             distance = np.sqrt((i - crow)**2 + (j - ccol)**2)
             if distance <= cutoff_freq:
-                masque[i, j] = 0
+                filtre[i, j] = 0
     
-    return masque
+    return filtre
+
+def transforme_de_fourier_inverse(dft_shift_filtered_channels):
+    image_filtree_channels = []
+    
+    for dft_shift_filtered in dft_shift_filtered_channels:
+        dft_filtered = np.fft.ifftshift(dft_shift_filtered)
+        image_filtree = np.fft.ifft2(dft_filtered)
+        image_filtree = np.abs(image_filtree)
+        image_filtree = np.uint8(255 * (image_filtree / np.max(image_filtree)))
+        image_filtree_channels.append(image_filtree)
+    
+    return cv2.merge(image_filtree_channels)
 
 
-def appliquer_filtre(dft_shift_filtered):
-    dft_filtered = np.fft.ifftshift(dft_shift_filtered)
-    image_filtree = np.fft.ifft2(dft_filtered)
-    image_filtree = np.abs(image_filtree)
+def butterworth_lowpass_filter(image, cutoff_freq, n_params_butter):
+    rows, cols, _ = image.shape
+    filtre = np.zeros((rows, cols), dtype=np.float32)
+    crow, ccol = rows // 2, cols // 2
     
-    image_filtree = np.uint8(255 * (image_filtree / np.max(image_filtree))) #normalize
+    for i in range(rows):
+        for j in range(cols):
+            distance = np.sqrt((i - crow)**2 + (j - ccol)**2)
+            filtre[i, j] = 1 / (1 + (distance / cutoff_freq)**(2 * n_params_butter))
     
-    return cv2.merge([image_filtree, image_filtree, image_filtree])
+    return filtre
+
+def butterworth_highpass_filter(image, cutoff_freq, n_params_butter):
+    rows, cols, _ = image.shape
+    filtre = np.ones((rows, cols), dtype=np.float32)
+    crow, ccol = rows // 2, cols // 2
+    
+    for i in range(rows):
+        for j in range(cols):
+            distance = np.sqrt((i - crow)**2 + (j - ccol)**2)
+            filtre[i, j] = 1 / (1 + (cutoff_freq / distance)**(2 * n_params_butter)) if distance != 0 else 1
+    
+    return filtre
