a847314fc874a6853bda0e54e2e4d790492fe601
oli-gh <113401495+oli-gh@users.noreply.github.com>
2025-04-11 13:57:46 -0400

    part 2 done

--- GTI411 - LAB 4/Lab4/models/transformation_model.py
+++ GTI411 - LAB 4/Lab4/models/transformation_model.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import cv2
 
 
 class TransformationModel:
@@ -36,7 +36,9 @@ class TransformationModel:
 
     def update_param(self, value, param):
         assert hasattr(self, param)
-        setattr(self, param, value)
+        current_type = type(getattr(self, param))
+        setattr(self, param, current_type(value))
+
 
 
     def set_image(self, image):
@@ -55,20 +57,86 @@ class TransformationModel:
 
     # TODO
     def draw_points(self, image, points):
-        # /!\ Attention à modifier uniquement image et pas self.image!
+        for (x, y) in points:
+            cv2.circle(image, (int(x), int(y)), radius=5, color=(0, 0, 255), thickness=-1)
+
+        for (start_idx, end_idx) in self.connections:
+            pt1 = tuple(points[start_idx])
+            pt2 = tuple(points[end_idx])
+            cv2.line(image, (int(pt1[0]), int(pt1[1])), (int(pt2[0]), int(pt2[1])), color=(0, 255, 0), thickness=2)
+
         return image
 
 
     # TODO
-    def apply_transformation(self, transformation:str):
+    def apply_transformation(self, transformation: str):
         assert transformation in ('translate', 'scale', 'shear', 'rotate')
         if self.image is None:
             return None
 
         print(f"Applying {transformation}")
-
-        image = self.image.copy() # <- /!\ Modifier image et pas self.image
+        image = self.image.copy()
         points = self.points.copy()
+
+        if transformation == 'translate':
+            M, points = self.translate_img(points)
+        elif transformation == 'scale':
+            M, points = self.scale_img(points)
+        elif transformation == 'shear':
+            M, points = self.shear_img(points)
+        elif transformation == 'rotate':
+            return self.rotate_img(image, points) #Retourne directement l'image comme on la place au milieux de l'écran contrairement aux autres fonctions
+            
+        transformed_image = cv2.warpAffine(image, M, (self.width, self.height), flags=cv2.INTER_LINEAR, borderValue=(255, 255, 255))
+        transformed_image = self.draw_points(transformed_image, points)
+
+        return transformed_image
+
+    def translate_img(self, points):
+        M = np.array([
+            [1, 0, self.trans_x],
+            [0, 1, self.trans_y]
+        ], dtype=np.float32)
+        return M, points + np.array([self.trans_x, self.trans_y])
+    
+
+    def scale_img(self, points):
+        M = np.array([
+            [self.scale_x, 0, 0],
+            [0, self.scale_y, 0]
+        ], dtype=np.float32)
+        return M, points * np.array([self.scale_x, self.scale_y])
+    
+    def shear_img(self, points):
+        M = np.array([
+            [1, self.shear, 0],
+            [0, 1, 0]
+        ], dtype=np.float32)
+
+        ones = np.ones((points.shape[0], 1))
+        points_h = np.hstack([points, ones])
+        sheared_points = np.dot(points_h, M.T)
+
+        return M, sheared_points
+    
+
+    def rotate_img(self, image, points):
+        h, w = self.base_image_shape
+        cx, cy = self.width // 2, self.height // 2 #centering
         
-        image = self.draw_points(image, points)
-        return image
+        M_center = np.array([
+            [1, 0, cx - w//2],
+            [0, 1, cy - h//2]
+        ], dtype=np.float32)
+        
+        centered_image = cv2.warpAffine(image, M_center, (self.width, self.height), borderValue=(255, 255, 255))
+        M_rotate = cv2.getRotationMatrix2D((cx, cy), self.rot_angle, 1.0)
+        transformed_image = cv2.warpAffine(centered_image, M_rotate, (self.width, self.height), borderValue=(255, 255, 255))
+        centered_points = self.points.copy() + np.array([cx - w//2, cy - h//2])
+  
+        ones = np.ones((centered_points.shape[0], 1))
+        points_h = np.hstack([centered_points, ones])
+        points = np.dot(points_h, M_rotate.T)
+        
+        transformed_image = self.draw_points(transformed_image, points)
+        return transformed_image
